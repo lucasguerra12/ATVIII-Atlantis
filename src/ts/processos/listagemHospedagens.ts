1fc0a4112b0edb7b1edda1e577f073b9a8b1b9fc
lucasguerra12 <lucasguerrawr2004@gmail.com>
2025-11-24 16:38:14 -0300

    feat: Add listagem de hospedagens process to main menu

---
+++ src/ts/processos/listagemHospedagens.ts
@@ -0,0 +1,29 @@
+import Processo from "../abstracoes/processo.js";
+import Armazem from "../dominio/armazem.js";
+
+export default class ListagemHospedagens extends Processo {
+    constructor() {
+        super();
+    }
+
+    processar(): void {
+        console.clear();
+        console.log('Iniciando listagem de hospedagens...');
+        
+        let hospedagens = Armazem.InstanciaUnica.Hospedagens;
+
+        if (hospedagens.length === 0) {
+            console.log('Não há hospedagens ativas no momento.');
+        } else {
+            console.log('----------------------------------------------------------------');
+            hospedagens.forEach((hospedagem, index) => {
+                console.log(`\n[Hospedagem ${index + 1}]`);
+                console.log(`Cliente: ${hospedagem.Cliente.Nome}`);
+                console.log(`Acomodação: ${hospedagem.Acomodacao.NomeAcomadacao}`);
+                console.log('----------------------------------------------------------------');
+            });
+        }
+
+        this.entrada.receberNumero('Pressione qualquer tecla e Enter para voltar...');
+    }
+}
